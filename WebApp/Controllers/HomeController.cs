67e9bdd85e736bf876ffdf82746ff89eebecd774
ben <bweake@hotmail.com>
2017-05-15 09:37:54 +1200

    Hi Boys The Names Jeff

---
+++ WebApp/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        public ActionResult Section1Index()
+        {
+            return View("Section1/Index");
+        }
+
+    }
+}
